238f314a29f3cd3883e30a8fc2831ba97ad82af7
TheNezzz <heinonen.johannes@gmail.com>
2024-11-17 00:24:19 +0200

    functional suitcloset and picking up

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -7,6 +7,9 @@ public class PauseMenu : MonoBehaviour
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
 
+    private void Start() {
+        Resume();
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
