336bf422ee609b3e8c3dc7bfd0974a7db9cd91c3
TheNezzz <heinonen.johannes@gmail.com>
2024-11-17 19:56:13 +0200

    last dialogues

--- Assets/ComputerPower.cs
+++ Assets/ComputerPower.cs
@@ -6,18 +6,31 @@ public class ComputerPower : MonoBehaviour
     Computer_Password _computerScript;
     Collider _collider;
     Light _computerLight;
+    bool _computerPowerEnabled = false;
     
 
     private void Start() {
-        _computerScript = GetComponent<Computer_Password>();
-        _collider = GetComponent<Collider>();
-        _collider.enabled = false;
-        _computerLight = GetComponentInChildren<Light>();
-        _computerLight.enabled = false;
-        _computerScript.enabled = false;
+        if (PersistantObjects.GameState.PowerOnline == false)
+        {
+            _computerScript = GetComponent<Computer_Password>();
+            _collider = GetComponent<Collider>();
+            _collider.enabled = false;
+            _computerLight = GetComponentInChildren<Light>();
+            _computerLight.enabled = false;
+            _computerScript.enabled = false;
+        }
+    }
+
+    private void Update() {
+        if(!_computerPowerEnabled && PersistantObjects.GameState.PowerOnline == true)
+        {
+            ComputerPowered();          
+        }
     }
 
     public void ComputerPowered() {
+        PersistantObjects.GameState.SetPowerOnline(true);
+        _computerPowerEnabled = true;
         _collider.enabled = true;
         _computerScript.enabled = true;
         _computerLight.enabled = true;
