bdeeb176fff3ef358332cf3684cd5c997c2d18e3
solitar86 <122887577+solitar86@users.noreply.github.com>
2024-11-17 21:27:41 +0200

    WinMenu korjattu????

--- Assets/Scripts/WinMenu.cs
+++ Assets/Scripts/WinMenu.cs
@@ -4,9 +4,21 @@ using System.Collections.Generic;
 
 public class WinMenu : MonoBehaviour
 {
+
+    public void Awake()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
     public void LoadMenu()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(0);
+    }
+
+    private void Update()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
